2a9fd45693f4ed6558849cc4dd916972519db816
LockedX <1130876885@qq.com>
2019-04-22 18:35:10 +0800

    自定义EnvironmentRepository和自动刷新义Environment

---
+++ spring-cloud-provider-zookeeper/src/main/java/com/lx/springcloudproviderzookeeper/controller/ServiceController.java
@@ -0,0 +1,21 @@
+package com.lx.springcloudproviderzookeeper.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ServiceController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("getAllService")
+    public List<String> getAllService() {
+        return discoveryClient.getServices();
+    }
+
+}
