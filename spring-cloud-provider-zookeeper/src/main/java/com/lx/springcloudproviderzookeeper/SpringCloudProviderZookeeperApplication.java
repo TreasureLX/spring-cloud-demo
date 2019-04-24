a032d0595ea3437700f185926306c148c53f7564
LockedX <1130876885@qq.com>
2019-04-24 10:52:40 +0800

    自定义EnvironmentRepository和自动刷新义Environment

---
+++ spring-cloud-provider-zookeeper/src/main/java/com/lx/springcloudproviderzookeeper/SpringCloudProviderZookeeperApplication.java
@@ -0,0 +1,15 @@
+package com.lx.springcloudproviderzookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudProviderZookeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudProviderZookeeperApplication.class, args);
+    }
+
+}
