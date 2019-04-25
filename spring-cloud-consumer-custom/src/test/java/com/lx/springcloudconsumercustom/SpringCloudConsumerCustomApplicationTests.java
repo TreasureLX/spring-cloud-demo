c120513433f7159e42070aa78403f384bd876aee
LockedX <1130876885@qq.com>
2019-04-25 16:45:43 +0800

    负载均衡自定义实现

---
+++ spring-cloud-consumer-custom/src/test/java/com/lx/springcloudconsumercustom/SpringCloudConsumerCustomApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lx.springcloudconsumercustom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudConsumerCustomApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
