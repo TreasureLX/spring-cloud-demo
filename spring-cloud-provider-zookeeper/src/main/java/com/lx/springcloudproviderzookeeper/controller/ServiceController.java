5968371bf0a1dfab4f080694909abedb59ab1a95
lanxing <1130876885@qq.com>
2019-05-14 00:21:36 +0800

    dd

--- spring-cloud-provider-zookeeper/src/main/java/com/lx/springcloudproviderzookeeper/controller/ServiceController.java
+++ spring-cloud-provider-zookeeper/src/main/java/com/lx/springcloudproviderzookeeper/controller/ServiceController.java
@@ -1,40 +1,33 @@
 package com.lx.springcloudproviderzookeeper.controller;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCollapser;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class ServiceController {
 
-    @Autowired
-    private DiscoveryClient discoveryClient;
+//    @Autowired
+//    private DiscoveryClient discoveryClient;
+//
+//    @GetMapping("getAllService")
+//    public List<String> getAllService() {
+//        return discoveryClient.getServices();
+//    }
 
-    @GetMapping("getAllService")
-    public List<String> getAllService() {
-        return discoveryClient.getServices();
-    }
-
-    @HystrixCommand(
-            fallbackMethod = "errorContent",
-            commandProperties = {
-                    @HystrixProperty(name = "",value = "00")
-            }
-    )
+//    @HystrixCommand(
+//            fallbackMethod = "errorContent",
+//            commandProperties = {
+//                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "5000")
+//            }
+//    )
     @GetMapping("hello")
     public String hello(@RequestParam String msg) {
         System.out.println(msg);
         return "hello "+msg;
     }
 
-    public String errorContent(){
+    public String errorContent(String message){
         return "fail";
     }
 }
