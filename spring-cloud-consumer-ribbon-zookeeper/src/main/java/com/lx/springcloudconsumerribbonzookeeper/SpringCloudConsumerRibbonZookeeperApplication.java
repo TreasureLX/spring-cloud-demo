f5a519d004706bf00242692c0af89ed25dbb5747
LockedX <1130876885@qq.com>
2019-05-17 18:32:43 +0800

    自定义Semaphore高级熔断

---
+++ spring-cloud-consumer-ribbon-zookeeper/src/main/java/com/lx/springcloudconsumerribbonzookeeper/SpringCloudConsumerRibbonZookeeperApplication.java
@@ -0,0 +1,16 @@
+package com.lx.springcloudconsumerribbonzookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lanxing
+ */
+@SpringBootApplication
+public class SpringCloudConsumerRibbonZookeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConsumerRibbonZookeeperApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-consumer-ribbon-zookeeper/src/test/java/com/lx/springcloudconsumerribbonzookeeper/SpringCloudConsumerRibbonZookeeperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lx.springcloudconsumerribbonzookeeper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudConsumerRibbonZookeeperApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
